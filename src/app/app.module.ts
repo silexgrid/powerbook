f97e77c7f2a8ee5d24b4d8a6d3c87ec4c4f1aa1c
codex-silexgrid <jsalazar@silexgrid.com>
2017-10-19 23:02:04 -0500

    Bottom navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule,MatIconModule,MatButtonModule,MatMenuModule,MatCardModule,MatInputModule,MatFormFieldModule,MatTabsModule} from '@angular/material';
+
+import { ExploreComponent } from './explore/explore.component';
+import { CreateComponent } from './create/create.component';
+import { ShareComponent } from './share/share.component';
+
+import {RouterModule, Routes} from '@angular/router';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { BottomNavbarComponent } from './bottom-navbar/bottom-navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExploreComponent,
+    CreateComponent,
+    ShareComponent,
+    TopNavbarComponent,
+    BottomNavbarComponent
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
@@ -21,7 +36,12 @@ import {MatToolbarModule,MatIconModule,MatButtonModule,MatMenuModule,MatCardModu
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
-    MatTabsModule
+    MatTabsModule,
+    RouterModule.forRoot([
+      { path: "explore", component: ExploreComponent },
+      { path: "create", component: CreateComponent },
+      { path: "share", component: ShareComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
